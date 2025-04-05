4e79442c4db33746f859743c907fbcf7994d9209
Astrak ... <vinitastrak04@gmail.com>
2025-04-05 18:20:22 +0530

    01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accounId = 1383753
+let accountEmail = "astrak@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState
+
+/*
+Preger not to use var
+because of issue in block scope and function scope
+*/
+
+console.log(accounId);
+console.log(accountEmail);
+console.table([accounId, accountEmail, accountPassword, accountCity]);
