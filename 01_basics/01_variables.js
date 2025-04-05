38f52ff653afd02f090e1a360a5c173e8ea4efd0
Astrak ... <vinitastrak04@gmail.com>
2025-04-05 18:49:20 +0530

    Add 01_variable.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,9 @@
 const accounId = 1383753
 let accountEmail = "astrak@google.com"
 var accountPassword = "12345"
-accountCity = "Delhi"
-let accountState
+accountCity = "Delhi"       //! not allowed
+let accountState            //Yes we don't to asign value while creating the variable.
+// Yes it is not necessary to use semi-colon at the end of the very line of code.
 
 /*
 Prefer not to use var
@@ -11,4 +12,4 @@ because of issue in block scope and function scope
 
 console.log(accounId);
 console.log(accountEmail);
-console.table([accounId, accountEmail, accountPassword, accountCity]);
+console.table([accounId, accountEmail, accountPassword, accountCity, accountState]);
