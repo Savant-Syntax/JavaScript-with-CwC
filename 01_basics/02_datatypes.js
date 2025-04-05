0f53ae3e72573277a9efd716d51d7464d8a5716d
Savant-Syntax <astrak242@gmaail.com>
2025-04-05 19:42:29 +0530

    Add datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3)    //we are using nodejs, not browser
+
+console.log(3 + 3) //This is good
+// console.log(4
+//     + 4)    //This is wrong, Reason => It reduces the readablility
+// console.log("Astrak");console.log("World");     //This is also reduce the readability
+
+console.log("Astrak")   //This is a good practice
+console.log("World")
+
+let name = "Astrak"
+let age = 18
+let isLoggedIn = false
+
+console.log([name, age, isLoggedIn])
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not asigned value
+// symbol => used for uniqueness
+// object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
