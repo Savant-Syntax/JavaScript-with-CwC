2054cb145d4c510aad4ffa45bc2e41e575cdfc8b
Savant-Syntax <astrakwo242@gmail.com>
2025-04-08 19:46:37 +0530

    part1 datatype-summnary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+//Datatypes are defind in 2-types: Primitive and Non-pritive
+
+//Primitive => Call by value => Change done in copy of code not reflected in actual code.
+//7 types: string, number, boolean, null, undefined, symbol, bigInt
+
+//Non-premitive => Call by reference => Change done in here reflected in the actual code.
+//tyles: array, object, functions
+
+//2 Types of languages: Static and Dynamic
+
+//Static => datatypes need to be defined while declaring the variables.
+
+//dyamic => we don't need this
+const age = 20; //here age acan be number, string etc
+//* That's why Javascript is a dyanimcally typed language
+
+
+//object
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "astrak",
+    age: 20,
+}
+
+
