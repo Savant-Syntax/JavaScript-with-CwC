463e72a25ff3c3b6a9db9fbcfd562fb3ab6d39cf
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-12 23:50:28 +0530

    dedicated way to asign number datatype

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,3 +1,6 @@
 const score = 400
 console.log(score); //automatically detect it is a number
 
+const balance = new Number(100)
+console.log(balance);   //specifically we defined the datatype is number
+
