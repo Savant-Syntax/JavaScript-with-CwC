99ebc5af780f985d241ba09aba91befd7d3e1686
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-12 23:52:23 +0530

    added some more fuctions about the datatype number

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -4,3 +4,8 @@ console.log(score); //automatically detect it is a number
 const balance = new Number(100)
 console.log(balance);   //specifically we defined the datatype is number
 
+console.log(balance.toString());    //coverting number into string
+console.log(typeof (balance));      //datatype is object
+console.log(balance.toFixed(2));    //100.00 => to avoid unneccessary output
+console.log(balance.toString().length);     //3
+
