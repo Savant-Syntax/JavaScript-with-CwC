60d285af12c0fde97bfe1026a66a2afb2a5d4047
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-13 23:31:11 +0530

    floor method

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -23,6 +23,8 @@ console.log(Math);
 console.log(Math.abs(-69)); //Absolute value => Change -ve value to +ve value, +ve remains
 console.log(Math.round(68.79345868));   //Round off 
 console.log(Math.ceil(4.2));    //Ceiling value => Upper value: 4.1, 4.2 => 5
+console.log(Math.floor(4.9));   //Floor value => Lower value: 4.9, 4.8 => 4
+
 
 
  
