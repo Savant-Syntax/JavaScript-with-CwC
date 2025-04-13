bd8425a990f2f943c1402feef18e9ac6fd53a153
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-13 23:35:43 +0530

    Max method

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -25,6 +25,8 @@ console.log(Math.round(68.79345868));   //Round off
 console.log(Math.ceil(4.2));    //Ceiling value => Upper value: 4.1, 4.2 => 5
 console.log(Math.floor(4.9));   //Floor value => Lower value: 4.9, 4.8 => 4
 console.log(Math.min(4, 6, 8, 10, 2, 3));   //Min value => 2
+console.log(Math.max(4, 6, 8, 10, 2, 3));   //Min value => 10
+
 
 
  
