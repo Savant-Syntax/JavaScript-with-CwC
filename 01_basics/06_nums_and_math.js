de37ab3997f082fcec3ebc7a19b4ea0796fab777
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-13 23:34:49 +0530

    Min method

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -24,7 +24,7 @@ console.log(Math.abs(-69)); //Absolute value => Change -ve value to +ve value, +
 console.log(Math.round(68.79345868));   //Round off 
 console.log(Math.ceil(4.2));    //Ceiling value => Upper value: 4.1, 4.2 => 5
 console.log(Math.floor(4.9));   //Floor value => Lower value: 4.9, 4.8 => 4
-
+console.log(Math.min(4, 6, 8, 10, 2, 3));   //Min value => 2
 
 
  
