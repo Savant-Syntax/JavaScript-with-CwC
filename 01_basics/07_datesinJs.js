65906f1207440d2491c1ef7198376cb15b12d5ff
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-14 20:29:04 +0530

    Homework

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -3,4 +3,9 @@ let myDate = new Date()
 console.log(myDate);    //not readable format
 console.log(myDate.toString());     //Indian Standard Time
 
-
+//Homework
+console.log(myDate.toDateString());     //Mon Apr 14 2025
+console.log(myDate.toISOString());      //2025-04-14T14:56:04.688z
+console.log(myDate.toJSON());       //2025-04-14T14:56:04.688z
+console.log(myDate.toLocaleDateString());   //14/4/2025
+console.log(myDate.toLocaleString());       //14/4/2025, 8:26:04 pm
