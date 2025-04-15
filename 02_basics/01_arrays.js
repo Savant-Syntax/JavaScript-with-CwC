69f2bc0e60f34c7c541bc9d3476356ea81066760
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-15 22:38:59 +0530

    type of elements doesn't matter

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,3 +1,8 @@
 //array
 const myArr = [0, 1, 2, 3, 4, 5]
 console.log(myArr);
+
+const myHeros = ["Iron-man", "Spider-man", "Hulk"]  //Anytypes of element can be fit in an array
+const Box1 = ["Falcon", 3, 6.88, 40]
+console.log(myHeros);
+console.log(Box1);
