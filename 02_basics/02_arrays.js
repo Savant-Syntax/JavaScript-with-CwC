ca90069d4427cff90984143d4f7a725c9ff46805
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-16 15:18:45 +0530

    add array inside an array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,5 @@
+const marvel_heros = ["Ironman", "Spiderman", "Thor", "Loki"]
+const dc_heros = ["Batman", "WonderWoman", "Flash", "Superman"]
+
+marvel_heros.push(dc_heros);       //Add array inside an array
+console.log(marvel_heros);
