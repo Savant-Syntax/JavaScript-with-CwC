5b48c57eb6064ed3f6a3336d4214080bd4dc275a
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-16 15:30:37 +0530

    concat use and difference between cincat and push

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,6 +1,7 @@
 const marvel_heros = ["Ironman", "Spiderman", "Thor", "Loki"]
 const dc_heros = ["Batman", "WonderWoman", "Flash", "Superman"]
 
+//*push
 marvel_heros.push(dc_heros);       //Add array inside an array
 console.log(marvel_heros);         //Here, dc_heros treated as an element not an array
 
@@ -8,3 +9,10 @@ console.log(marvel_heros);         //Here, dc_heros treated as an element not an
 //Basically, It is how to access an array which is inside an array
 
 console.log(marvel_heros[4][1]);    //WonderWoman => 2D array concept
+
+//*concat
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
+//push work on existing array
+//concat combine 2-array and return a new array
+
