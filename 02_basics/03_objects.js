2e1b8e2462eb29b8ffb6471ccccd0d83881a50c1
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-17 21:46:46 +0530

    object is started

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,8 @@
+//Objects can be created by 2-ways:
+//1. Constructor
+//2. Literals
+
+//If obejct is created from
+//---constructor---> It forms singleton
+//---literals---> It doesn't form singleton
+
