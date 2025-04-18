963a437a47bb1f35f079ca3160edec0dab243656
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-18 21:58:36 +0530

    Object.assign()

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,4 +1,4 @@
-//object can be define by literal and constructors
+//Object can be define by literal and constructors
 //Here, we gonna learn how to create object from constructors
 
 const tinderUser1 = new Object();
@@ -14,7 +14,7 @@ tinderUser.isLoggedIn = false
 
 console.log(tinderUser);
 
-//object nesting
+//*Object nesting
 const regularUser = {
     email: "some@gmail.com",
     fullname: {
@@ -29,9 +29,16 @@ console.log(regularUser.fullname.userfullname.firstname);
 //optional chaning
 //console.log(regularUser.fullname?.userfullname.firstname);
 
-//object combination
+//*object combination
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
 
 const obj3 = { obj1, obj2 }
 console.log(obj3);      //one large object has the whole other data like an array method
+
+//*Object.assign()
+//const obj_3 = Object.assign(obj1, obj2)     //not a proper way
+const obj_3 = Object.assign({}, obj1, obj2, obj4)     //proper way => target, sourceS
+console.log(obj_3)      //{} => act as target, others => act as source
+//If you are not writing {} then the whole value is getting stored in obj1
