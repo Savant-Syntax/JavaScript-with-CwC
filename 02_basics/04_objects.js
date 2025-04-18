59d3b1eba484b6af904dbaf0031cfeb60f234cfc
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-18 20:52:49 +0530

    singleton and non-singleton obj

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,8 @@
+//object can be define by literal and constructors
+//Here, we gonna learn how to create object from constructors
+
+const tinderUser1 = new Object();
+console.log(tinderUser1);    //empty obj => singleton
+
+const tinderUser2 = {}
+console.log(tinderUser2);   //empty obj => non-singleton
