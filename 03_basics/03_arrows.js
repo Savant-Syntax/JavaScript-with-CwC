3784321271e48d32af7f9188db81e0c1c514bcdb
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-22 23:01:10 +0530

    this. refers empty obj in node environment

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -4,6 +4,8 @@ const user = {
 
     welcomeMessage: function () {
         console.log(`${this.username}, welcome to website`);    //"this." it is used for accessing the current context
+        console.log(this);      //prints the whole current context of the given object.
+        
     }
 
 }
@@ -13,3 +15,5 @@ user.username = "Sam"   //Changing the value.
 user.welcomeMessage()   //Changed value => Sam
 //How did we access the changed value?
 //=> this. used to access the current value so when we changed the value the changed value became the current value for the context.
+
+console.log(this);  // {} => empty object, but why? In node it refers to an empty object because now, there is no context inside global object.
