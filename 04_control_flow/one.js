50a19d47c66d362b891cfdf1742e54009a578aa9
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-23 23:32:43 +0530

    basic comparison optr

---
+++ 04_control_flow/one.js
@@ -0,0 +1,21 @@
+// if
+
+if (true) {
+    //code will be executed
+}
+if (false) {
+    //code will not be executed
+}
+
+//How we will know the condition is true or false? => By comparison
+
+const isUserloggedIn = true
+if (2 == "2") {
+    console.log("executed");  
+}
+if (2 === "2") {
+    console.log("executed");  
+}
+
+//some operator for comparison => "<, >, <=, >=, ==, !=, ===, !=="
+//"=" single equal to use assign value "==" double equal is use to check the value is equal.
