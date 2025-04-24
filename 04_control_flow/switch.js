39441a84f98a9ce1c1fc374795c86f032f4cf232
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-24 22:37:21 +0530

    Basics of switch for string

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -28,3 +28,23 @@ switch (month) {
         
         break;
 }
+
+//In string case
+const Month = "mar"
+switch (Month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Febuary");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+
+    default:
+        break;
+}
