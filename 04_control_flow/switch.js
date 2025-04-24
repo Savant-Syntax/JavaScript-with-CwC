20e789441c8e5870ede0aab1f830d03dbf4ac819
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-24 22:32:36 +0530

    Basics of switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+//         break;
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:                     //key matched => Entire code below it will executed //*except default
+        console.log("Mar");
+        break;                  //break save us form executing the rest of the code lines
+    case 4:
+        console.log("Apr");
+        break;
+
+    default:                    //seems like else in if-else condition
+        console.log("default case match");
+        
+        break;
+}
