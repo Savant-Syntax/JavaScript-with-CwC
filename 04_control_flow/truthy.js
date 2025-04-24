5ece29a121308764df51752506ba1c6ad303abd6
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-24 22:49:30 +0530

    Empty Array

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -10,3 +10,13 @@ if (userEmail) {
 //Basically there are some values we assume that they represent false.
 //falsy values
 //false, 0, -0, BigIng 0n, "", null, undefined, NaN
+
+//Interesting truthy values
+// "0", "false", " ", [], {}, function(){}
+
+//How to check an array is empty or not?
+const array = []
+if (array.length === 0) {
+    console.log("Array is empty");
+    
+}
