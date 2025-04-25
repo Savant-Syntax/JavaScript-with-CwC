58fb6f3177ff5ee24451c3316f7dc51c822f114a
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-25 20:28:10 +0530

    1 to 10 tables

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -13,9 +13,9 @@ for (let i = 0; i <= 10; i++) {
     // console.log(element);   
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 1; i <= 10; i++) {
     console.log(`Outer loop value ${i}`);
-    for (let j = 0; j < 10; j++) {
+    for (let j = 1; j <= 10; j++) {
     //    console.log(`Inner loop value ${j} andd inner loop ${i}`);
        console.log(i + '*' + j + ' = ' + i*j);
        
