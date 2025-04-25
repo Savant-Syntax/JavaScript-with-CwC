995ba2e86512b09611efca40c843a06747eed6d2
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-25 20:34:00 +0530

    loop on array

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -14,12 +14,24 @@ for (let i = 0; i <= 10; i++) {
 }
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`Outer loop value ${i}`);
+    //console.log(`Outer loop value ${i}`);
     for (let j = 1; j <= 10; j++) {
-    //    console.log(`Inner loop value ${j} andd inner loop ${i}`);
-       console.log(i + '*' + j + ' = ' + i*j);
+        //console.log(`Inner loop value ${j} andd inner loop ${i}`);
+        //console.log(i + '*' + j + ' = ' + i*j);
        
         
     }
+    
+}
+
+
+//loop on array
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+    
     
 }
