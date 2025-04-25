e739ef6231af07d357e1b2d3085454c3da63ed1b
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-25 20:25:00 +0530

    two index in one for loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -13,3 +13,13 @@ for (let i = 0; i <= 10; i++) {
     // console.log(element);   
 }
 
+for (let i = 0; i < 10; i++) {
+    console.log(`Outer loop value ${i}`);
+    for (let j = 0; j < 10; j++) {
+    //    console.log(`Inner loop value ${j} andd inner loop ${i}`);
+       console.log(i + '*' + j + ' = ' + i*j);
+       
+        
+    }
+    
+}
