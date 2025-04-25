3a07cc0c87f8417a6bae784212dfbaac09a9328a
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-25 20:08:53 +0530

    for loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,10 @@
+// for loop
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    console.log(element);   
+}
