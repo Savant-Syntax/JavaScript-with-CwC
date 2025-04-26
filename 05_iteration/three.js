0c3042c2c4bbc782f133009d398743e4a75840e5
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-26 23:07:04 +0530

    for of loop

---
+++ 05_iteration/three.js
@@ -0,0 +1,9 @@
+//for of loop
+// for (const element of object) {
+    
+// }
+
+const arr = [1, 2, 3, 4, 5]
+for (const num of arr) {                                        //object => on which item you are going to do work
+    console.log(num);                                           //num => iteration, arr => object   
+}
