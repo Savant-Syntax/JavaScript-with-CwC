b2134a2f56af8d593397e9641fc601d7bf82034f
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-26 20:44:29 +0530

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,12 @@
+//while and do while loop
+
+//while loop
+// while (condition) {
+    
+// }
+let index = 0
+while (index <= 10) {                                               //intialization
+    console.log(`value of index is ${index}`);
+    index = index + 2                                               //termination
+}
+
