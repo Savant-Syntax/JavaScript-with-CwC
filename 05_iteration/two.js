26f2a3f41095bcb32aa2d50651be19bcc40792cf
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-26 20:52:40 +0530

    do while loop

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -6,7 +6,7 @@
 // }
 let index = 0
 while (index <= 10) {                                               //intialization
-    console.log(`value of index is ${index}`);
+    // console.log(`value of index is ${index}`);
     index = index + 2                                               //termination
 }
 
@@ -14,6 +14,17 @@ while (index <= 10) {                                               //intializat
 let myArray = ['flash', 'batman', 'superman']
 let arr = 0
 while (arr < myArray.length) {
-    console.log(`value is ${myArray[arr]}`);
+    // console.log(`value is ${myArray[arr]}`);
     arr = arr + 1                                                   //arr = arr + 1 === shortform ===> arr++
 }
+
+//do while loop
+// do {
+    
+// } while (condition);
+// let score = 1                                                    //normal case
+let score = 11                                                      //feature => implemetation first then check the condition (at first time)
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
