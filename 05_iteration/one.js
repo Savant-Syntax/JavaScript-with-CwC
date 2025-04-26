271b86bba91d7f0c51a35cd2243a1d5d3899a16f
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-26 20:40:19 +0530

    completed

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -54,7 +54,6 @@ for (let i = 1; i <= 20; i++) {
 for (let i = 1; i <= 20; i++) {
     if (i == 5) {
         console.log(`Detected 5`);
-        co
         
     }
     console.log(`value of i is ${i}`);
