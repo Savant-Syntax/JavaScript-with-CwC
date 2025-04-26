ce29eaf2d1a2c1058e2ba96193ad1a5930d8a2f2
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-26 23:09:20 +0530

    for of loop on string

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -5,5 +5,13 @@
 
 const arr = [1, 2, 3, 4, 5]
 for (const num of arr) {                                        //object => on which item you are going to do work
-    console.log(num);                                           //num => iteration, arr => object   
+    // console.log(num);                                           //num => iteration, arr => object   
+}
+
+//example
+//for of loop on a string
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(`Each character is ${greet}`);
+    
 }
