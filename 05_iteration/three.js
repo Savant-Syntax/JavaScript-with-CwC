147f60446dbf517808e5fd3ebd8c04fa9f737ab4
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-26 23:14:29 +0530

    map

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -5,13 +5,21 @@
 
 const arr = [1, 2, 3, 4, 5]
 for (const num of arr) {                                        //object => on which item you are going to do work
-    // console.log(num);                                           //num => iteration, arr => object   
+    // console.log(num);                                        //num => iteration, arr => object   
 }
 
 //example
 //for of loop on a string
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    console.log(`Each character is ${greet}`);
+    // console.log(`Each character is ${greet}`);
     
 }
+
+//maps
+const map = new Map                                             //maps holds key value pairs and remembers the original insertion order of the keys                           
+map.set('IN', 'India')
+map.set('USA', 'United State of America')
+map.set('FR', 'France')
+map.set('IN', 'India')                                          //unique value only and maintain the order
+console.log(map);
