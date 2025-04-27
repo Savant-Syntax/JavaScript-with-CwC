eb58e6c5fefacaa6a4826e6f29fbd9c5683acd59
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-27 22:50:10 +0530

    refering other function in forEach loop

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -1,12 +1,21 @@
 const coding = ['js', 'ruby', 'java', 'python', 'cpp']
 coding.forEach(function (val) {                                 //val can be any name
-    console.log(val);
+    // console.log(val);
     
 })                                                              //In call back function we donot have to write the name of the function
 
 
 //with arrow function
 coding.forEach((item) => {                                      //item can be anyname
-    console.log(item);
+    // console.log(item);
     
 })
+
+
+//++++++++ Interesting +++++++++
+function printMe(item) {
+    console.log(item);
+    
+}
+
+coding.forEach(printMe);                                        //We are not calling the function we are refering the function
