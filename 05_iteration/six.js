90bfc12571d106a70106fc6934b598aade3b626e
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-27 23:10:14 +0530

    Is forEach return anyvalue?

---
+++ 05_iteration/six.js
@@ -0,0 +1,8 @@
+const coding = ['js', 'ruby', 'java', 'python', 'cpp']
+//Can we store those values in a variable?
+const values = coding.forEach((item) => {                   //yes
+    console.log(item);
+    return item                                             //it return nothing 
+})
+
+console.log(values);                                        //undefined
