3b1b0172e8aac3603bb79f923e331fa937eea574
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-27 22:40:26 +0530

    for in loop on map

--- 05_iteration/four.js
+++ 05_iteration/four.js
@@ -25,3 +25,18 @@ for (const key in programming) {
     // console.log(programming[key]);               //values
     
 }   //yes, it works
+
+
+//Is for in loop will on map?
+const map = new Map
+map.set('IN', 'India')
+map.set('USA', 'United State of America')
+map.set('FR', 'France')
+map.set('IN', 'India')
+
+for (const key in Map) {
+    console.log(key);
+    
+}
+//Output: nothing
+//But also no error => Reason: Map is not iterable here
