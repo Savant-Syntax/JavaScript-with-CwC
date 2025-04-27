d162194367314e791c6ed0bf9ef611755c53a851
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-27 22:47:30 +0530

    forEach loop

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -3,3 +3,10 @@ coding.forEach(function (val) {                                 //val can be any
     console.log(val);
     
 })                                                              //In call back function we donot have to write the name of the function
+
+
+//with arrow function
+coding.forEach((item) => {                                      //item can be anyname
+    console.log(item);
+    
+})
