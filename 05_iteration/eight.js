1b81d5a194ccb0d1c64b447bd6d864b67eeb489d
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-28 22:34:05 +0530

    reduce method

---
+++ 05_iteration/eight.js
@@ -0,0 +1,16 @@
+//reduce
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+//1st iteration of loop => accumulator = initialValue, acculamulator + currentValue (1st element of the array)
+//2nd iteration of loop => accumulator (which is intitialValue + currentValue) + initialValue (2nd element of the array)
+//3rd and further more iteration of loop => accumulator = accumulator + currentValue
+
+console.log(sumWithInitial);
+// Expected output: 10
