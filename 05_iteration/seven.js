d3b741900593f6d147b86287f33e026b9c1d1bd5
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-28 22:23:01 +0530

    chaining

--- 05_iteration/seven.js
+++ 05_iteration/seven.js
@@ -1,8 +1,19 @@
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 //Q1. Add 10 value each element of the given array
-const newNums = myNumbers.map((num) => num + 10)
-console.log(newNums);           //map return the value so it is easy to use
+// const newNums = myNumbers.map((num) => num + 10)
+// console.log(newNums);           //map return the value so it is easy to use
 
 //if you open scope the output will be undefined
 //for that you have to write the return
+
+
+//Chaining
+//1. Using multiple method in a continuous manner
+const newNums = myNumbers
+                .map((num) => num * 5)      //its production will pass on the second chain
+                .map((num) => num + 1)
+                .filter((num) => num >= 40)
+                
+                
+console.log(newNums);
