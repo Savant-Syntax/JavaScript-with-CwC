da218638d27b0bd37a4936fcb9104dc1ba51d6e0
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-28 22:41:47 +0530

    accumulator and currentValue

--- 05_iteration/eight.js
+++ 05_iteration/eight.js
@@ -14,3 +14,12 @@ const sumWithInitial = array1.reduce(
 
 console.log(sumWithInitial);
 // Expected output: 10
+
+//Easy example
+const myNums = [1, 2, 3]
+const myTotal = myNums.reduce(function (acc, currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    
+    return acc + currval
+}, 3)
+console.log(myTotal);
