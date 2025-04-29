47530122eb4706d20f881de1a4a8fc5c8872c73d
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-29 21:49:11 +0530

    splice() -> remove, add, replace

--- question/one.js
+++ question/one.js
@@ -10,3 +10,16 @@ myArr_1.unshift(8)
 //console.log(myArr_1);
 myArr_1.shift()
 //console.log(myArr_1);
+
+//? What happens when you use the splice method on an array?
+// splice is used to add, remove and replace the values of an array.
+// After modifiction the old array got changed
+
+const myArr_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+myArr_2.splice(1, 6)    //remove
+// console.log(myArr_2);
+myArr_2.splice(1, 2, 2, 3, 4, 5, 6, 7)
+//console.log(myArr_2);   //add
+myArr_2.splice(1, 3, "a", "b", "c")
+//console.log(myArr_2);   //replace
+
