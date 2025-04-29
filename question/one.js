587729388d7844578e82ca0f7604c8bef489c529
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-29 21:34:47 +0530

    unshift v/s shift

--- question/one.js
+++ question/one.js
@@ -1,3 +1,12 @@
 //? What is the purpose of an array in JavaScript?
 // To store multiple items in a single variable with multiple datatypes and manipulation of data can be done by their index value, which starts from 0.
 
+//? Explain the difference between the push and unshift methods.
+// unshift => add value at the starting of the array
+// shift => remove value from the starting of the array
+
+const myArr_1 = [1, 2, 3, 4, 5, 6, 7]
+myArr_1.unshift(8)
+//console.log(myArr_1);
+myArr_1.shift()
+//console.log(myArr_1);
