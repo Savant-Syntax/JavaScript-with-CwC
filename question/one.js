5474423a76b7765d202a01431d48f3ebc5bd44a7
Savant-Syntax <vinitastrak04@gmail.com>
2025-04-29 21:28:00 +0530

    purpose of array in js

---
+++ question/one.js
@@ -0,0 +1,3 @@
+//? What is the purpose of an array in JavaScript?
+// To store multiple items in a single variable with multiple datatypes and manipulation of data can be done by their index value, which starts from 0.
+
