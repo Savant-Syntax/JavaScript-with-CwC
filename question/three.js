065a28bd4df760c14a7c7b427fdd84cc7752b6cd
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-01 23:04:50 +0530

    Object literals

--- question/three.js
+++ question/three.js
@@ -22,3 +22,38 @@ const obj2 = { name: "Jane" }; // obj1 and obj2 are distinct objects.
 //console.log(obj1);
 //console.log(obj2);
 
+
+//? Object literals
+// Use curly braces {}.
+// Keys can be strings, symbols, or variables.
+// Values can be any type (primitive, array, function, etc.).
+
+const person1 = {
+    name: "Astrak",
+    age: 20,
+    isEmployed: true,
+};
+// console.log(person1); 
+
+const key = "job";
+const person2 = {
+    "full-name": "Astrak",  // String key
+    [key]: "Developer",       // Variable key
+    [Symbol("id")]: 1234,     // Symbol key -> makes things unique
+};
+// console.log(person2); 
+
+const person3 = {
+    name: "Alice",
+    age: 25,
+    skills: ["JavaScript", "CSS", "HTML"],  // Array
+    greet: function() {                     // Function
+        console.log("Hello!");
+    },
+    address: {                              // Nested Object
+        city: "Delhi",
+        zip: 10001,
+    },
+};
+//erson3.greet();
+// console.log(person3.address.city);
