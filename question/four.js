0d03304be6dc42aafde10caedbfe5592e09196c6
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-02 20:55:02 +0530

    Using symbols as a key

--- question/four.js
+++ question/four.js
@@ -6,3 +6,13 @@ const sym1 = Symbol("description1");
 const sym2 = Symbol("description2");
 
 // console.log(sym1 === sym2);     //false
+
+//? Using symbols as a key
+const symKey = Symbol("uniqueKey");
+const person = {};
+
+// Adding a symbol key
+person[symKey] = "Symbol Value";
+
+// console.log(person);
+// console.log(person[symKey]); 
