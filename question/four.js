58bfdbdbfa939fddc9e9eaad36babf3992f971f2
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-02 20:51:27 +0530

    Creating symbols

---
+++ question/four.js
@@ -0,0 +1,8 @@
+//? Symbol as a key
+// Symbols are unique and immutable primitive values in JavaScript, often used as property keys to avoid naming collisions.
+
+//? Creating Symbols
+const sym1 = Symbol("description1");
+const sym2 = Symbol("description2");
+
+console.log(sym1 === sym2);     //false
