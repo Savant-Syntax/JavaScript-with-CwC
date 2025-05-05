4291559722338d8c8aa578ba7e056807c10a46e2
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-05 21:39:33 +0530

    Object creation

---
+++ question/six.js
@@ -0,0 +1,20 @@
+//? Object Creation:
+//? Create an object student using both new Object() and object literal syntax. Add properties name, age, and grade, then log both objects.
+
+//construuctor
+const student1 = new Object();
+student1.name = "John";
+student1.age = 20;
+student1.grade = "A";
+
+//console.log(student1);
+
+//Literals
+const student2 = {
+    name: "Jane",
+    age: 22,
+    grade: "B"
+  };
+  
+//console.log(student2);
+  
