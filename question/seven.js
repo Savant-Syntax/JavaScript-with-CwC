661808952862b601ca75df377d81691da928a652
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-20 23:06:37 +0530

    Flattened array

--- question/seven.js
+++ question/seven.js
@@ -1484,7 +1484,7 @@ function flattenArray(nestedArray) {
   }, []);
 }
 
-console.log(flattenArray([[1, 2], [3, [4, 5]], 6])); // Output: [1, 2, 3, 4, 5, 6]
-console.log(flattenArray([1, [2, [3, [4, [5]]]]])); // Output: [1, 2, 3, 4, 5]
-console.log(flattenArray([])); // Output: []
-console.log(flattenArray([1, 2, 3])); // Output: [1, 2, 3]
+// console.log(flattenArray([[1, 2], [3, [4, 5]], 6])); // Output: [1, 2, 3, 4, 5, 6]
+// console.log(flattenArray([1, [2, [3, [4, [5]]]]])); // Output: [1, 2, 3, 4, 5]
+// console.log(flattenArray([])); // Output: []
+// console.log(flattenArray([1, 2, 3])); // Output: [1, 2, 3]
