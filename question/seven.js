dfc20959a1a5a63ac54f4649e343cd0bf5dbbda8
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-25 23:31:45 +0530

    Array question-2

--- question/seven.js
+++ question/seven.js
@@ -2142,4 +2142,4 @@ function moveZeroes(arr) {
   return arr;
 }
 
-console.log(moveZeroes([0, 1, 0, 3, 12])); // Output: [1, 3, 12, 0, 0]
+// console.log(moveZeroes([0, 1, 0, 3, 12])); // Output: [1, 3, 12, 0, 0]
