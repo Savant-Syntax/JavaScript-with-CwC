9a5da339357419cc0ec38acd8207386a28afe6b0
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-27 22:52:16 +0530

    question-1

--- question/seven.js
+++ question/seven.js
@@ -2275,4 +2275,4 @@ function rotateArray(arr, k) {
   return arr.slice(-k).concat(arr.slice(0, -k));
 }
 
-console.log(rotateArray([1, 2, 3, 4, 5], 2)); // Output: [4, 5, 1, 2, 3]
+// console.log(rotateArray([1, 2, 3, 4, 5], 2)); // Output: [4, 5, 1, 2, 3]
