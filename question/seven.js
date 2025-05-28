5a1e0cf2298a462817bd1f74383f5e0f2037ab51
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-28 23:26:09 +0530

    Rearrange array alternately

--- question/seven.js
+++ question/seven.js
@@ -2421,4 +2421,4 @@ function findPairs(arr, target) {
   return pairs;
 }
 
-console.log(findPairs([1, 2, 3, 4, 5], 6)); // Output: [[2, 4], [1, 5]]
+// console.log(findPairs([1, 2, 3, 4, 5], 6)); // Output: [[2, 4], [1, 5]]
