c38444d07062b4526d6eacc14d1cf4a8ce65a099
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-29 23:21:38 +0530

    Rotate array by k step

--- question/seven.js
+++ question/seven.js
@@ -2428,4 +2428,4 @@ function rotateArray(arr, k) {
   return arr.slice(-k).concat(arr.slice(0, -k));
 }
 
-console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 3)); // Output: [5, 6, 7, 1, 2, 3, 4]
+// console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 3)); // Output: [5, 6, 7, 1, 2, 3, 4]
