fc241f34423fe8aa72fa2a1dbcbcf311b936e42a
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-29 23:24:01 +0530

    Merge two sorted array

--- question/seven.js
+++ question/seven.js
@@ -2451,4 +2451,4 @@ function mergeSortedArrays(arr1, arr2) {
   return [...new Set(merged)].sort((a, b) => a - b);
 }
 
-console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
+// console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
