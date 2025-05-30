648ae86b66b56f2a184b319824479a0fcaef2d6c
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-30 22:34:08 +0530

    Find the longest consective sequence

--- question/seven.js
+++ question/seven.js
@@ -2520,4 +2520,4 @@ function longestConsecutive(arr) {
   return maxStreak;
 }
 
-console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // Output: 4
+// console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // Output: 4
