cb82f05809d354078efeaf961b8f87e02011abf7
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-31 23:00:09 +0530

    find Duplicates in an array

--- question/eight.js
+++ question/eight.js
@@ -5,3 +5,21 @@ function findKthLargest(arr, k) {
 }
 
 // console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); // Output: 5
+
+// Find Duplicates in an Array
+function findDuplicates(arr) {
+  let seen = new Set();
+  let duplicates = new Set();
+
+  for (let num of arr) {
+    if (seen.has(num)) {
+      duplicates.add(num);
+    } else {
+      seen.add(num);
+    }
+  }
+
+  return Array.from(duplicates);
+}
+
+console.log(findDuplicates([4, 3, 2, 7, 8, 2, 3, 1])); // Output: [2, 3]
