50241fad5d2ebc8b560de84494dc669e0be3747b
Savant-Syntax <vinitastrak04@gmail.com>
2025-05-31 22:58:46 +0530

    find the  Kth largest element

---
+++ question/eight.js
@@ -0,0 +1,7 @@
+// Find the Kth Largest Element
+function findKthLargest(arr, k) {
+  arr.sort((a, b) => b - a);
+  return arr[k - 1];
+}
+
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); // Output: 5
