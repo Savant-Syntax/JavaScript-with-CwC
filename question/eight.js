ee49891888183a32759111782686abbd545fcfa8
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-02 20:31:41 +0530

    Rotate array

--- question/eight.js
+++ question/eight.js
@@ -148,5 +148,4 @@ function rotateArray(arr, k) {
   k = k % arr.length;
   return [...arr.slice(-k), ...arr.slice(0, -k)];
 }
-
-console.log(rotateArray([1, 2, 3, 4, 5], 2)); // Output: [4, 5, 1, 2, 3]
+// console.log(rotateArray([1, 2, 3, 4, 5], 2)); // Output: [4, 5, 1, 2, 3]
