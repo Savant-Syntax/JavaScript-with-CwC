dae6b39fde62ce17dac7fbdc237585cbca4f9b34
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-02 20:33:57 +0530

    Find all pairs with a given sum

--- question/eight.js
+++ question/eight.js
@@ -175,4 +175,4 @@ function findPairsWithSum(arr, target) {
   return pairs;
 }
 
-console.log(findPairsWithSum([1, 2, 3, 4, 5], 6)); // Output: [[2, 4], [1, 5]]
+// console.log(findPairsWithSum([1, 2, 3, 4, 5], 6)); // Output: [[2, 4], [1, 5]]
