eb92e81f8e8707337ed95fbb403913dc6e864407
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-04 22:39:00 +0530

    Partition array into even and odd

--- question/eight.js
+++ question/eight.js
@@ -247,5 +247,5 @@ function partitionArray(arr) {
   return { even, odd };
 }
 
-console.log(partitionArray([1, 2, 3, 4, 5, 6]));
+// console.log(partitionArray([1, 2, 3, 4, 5, 6]));
 // Output: { even: [2, 4, 6], odd: [1, 3, 5] }
